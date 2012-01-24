d19c8e2fdc65e5df4b71ef0f6a9c63325335cf69
charlesmarshall <cm56marshall@googlemail.com>
2012-01-24 10:53:39 +0000

    other file types that vimeo will allow

--- setup.php
+++ setup.php
@@ -1,4 +1,8 @@
 <?
 WildfireMedia::$classes[] = 'WildfireVimeoFile';
 WildfireMedia::$allowed['mp4'] = 'WildfireVimeoFile';
+WildfireMedia::$allowed['asf'] = 'WildfireVimeoFile';
+WildfireMedia::$allowed['mpeg'] = 'WildfireVimeoFile';
+WildfireMedia::$allowed['avi'] = 'WildfireVimeoFile';
+WildfireMedia::$allowed['wmv'] = 'WildfireVimeoFile';
 ?>
